17599aa63ce1ba2822c13b041c30e53a8ec9ed73
Daniel Araujo <31084711+araujodm@users.noreply.github.com>
2018-07-20 14:27:17 -0300

    Formatting DH table

--- RoboND-Kinematics-Project/IK_server.py
+++ RoboND-Kinematics-Project/IK_server.py
@@ -30,9 +30,9 @@ def handle_calculate_IK(req):
        
 
         # DH Parameters
-        DH_Table = {alpha0:0, a0:0, d1:0.75, q1: q1,
-            alpha1: -pi/2,  a1:   0.35, d2:     0, q2: -pi/2 + q2,
-            alpha2:     0,  a2:   1.25, d3:     0, q3:         q3,
+        DH_Table = {alpha0:     0,  a0:      0, d1:  0.75, q1:         q1,
+           			alpha1: -pi/2,  a1:   0.35, d2:     0, q2: -pi/2 + q2,
+            		alpha2:     0,  a2:   1.25, d3:     0, q3:         q3,
                     alpha3: -pi/2,  a3: -0.054, d4:   1.5, q4:         q4,
                     alpha4:  pi/2,  a4:      0, d5:     0, q5:         q5,
                     alpha5: -pi/2,  a5:      0, d6:     0, q6:         q6,
